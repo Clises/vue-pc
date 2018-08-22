71f1bbc601dc843f2801c969cac6fc6eaf5921e4
EDZ <chenglin.gao@plusx.cn>
2018-08-22 18:39:07 +0800

    ·

---
+++ src/components/Menu.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-aside width='200px'>
+      <div class="menu">
+      </div>
+  </el-aside>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+
+        },
+        computed: {},
+        methods: {},
+        components: {},
+        mounted() {
+
+        }
+    }
+</script>
+
+<style lang="scss" type="text/scss">
+  .menu{
+  }
+</style>
